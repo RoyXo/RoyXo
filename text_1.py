33b6e6be9cc31b52607d75291026ccd2e0897a14
RoyXo <nonxcopy@gmail.com>
2022-03-07 21:50:48 +0200

    ok2!

--- text_1.py
+++ text_1.py
@@ -1,2 +1,14 @@
 # 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+
+def my_func(x, y):
+    try:
+        return int(x)/int(y)
+    except ZeroDivisionError:
+        print("Division by zero is not possible.")
+    except ValueError:
+        print("You should inter integers.")
+
+
+print(my_func(input(), input()))
